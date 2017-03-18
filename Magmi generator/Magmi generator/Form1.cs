d642b0c3e78989ab916d2c2bc3d255e00bc901bc
Clement Gamache <clementgamache@gmail.com>
2017-03-18 11:45:04 -0400

    A project that is more simple for a clerk to use to import frame products for our Clerk

---
+++ Magmi generator/Magmi generator/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Magmi_generator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            if (folderBrowserDialog1.SelectedPath.Length > 0)
+                label1.Text = folderBrowserDialog1.SelectedPath;
+            else
+                label1.Text = "No file chosen";
+        }
+    }
+}
